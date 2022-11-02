4a847a9e2cca86ae52e8f09b1e73f7e72e75ee65
L. Krohn <LNKrohn@gmail.com>
2022-11-02 01:04:21 -0400

    finish basic app functions

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const library = [
         "title": "Cat's Cradle",
         "author": "Kurrt Vonnnegut",
         "pages": "999",
-        "read": false
+        "read": true
         },
     {
         "title": "goofy movie",
@@ -19,19 +19,46 @@ const library = [
             }
     ];
 
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = Boolean(read);
-// }
+const resetBookCards = () => {
+    document.getElementById('container').innerHTML = '';
+}
+
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = isRead;
+    }
+}
+
+const getBookFromInput = () => {
+    const title = document.getElementById('bookTitle').value
+    const author = document.getElementById('bookAuthor').value
+    const pages = document.getElementById('bookPages').value
+    const isRead = document.getElementById('bookRead').checked
+    return new Book(title, author, pages, isRead)
+  }
+
+
+function openForm() {
+    document.getElementById("formContainer").style.display = "block";
+  }
+  
+function closeForm() {
+    document.getElementById("formContainer").style.display = "none";
+  }
+
 
 const container = document.querySelector('#container');
 
 function createBookCards () {
     for (let i=0; i<library.length; i++) {
         const book = document.createElement('div');
-            book.classList.add('book');
+            book.classList.add('book', `book-${[i]}`);
+            book.id = `book-${[i]}`;
+            let bookIndex = i;
+            console.log(bookIndex);
         const title = document.createElement('div');
             title.classList.add('title', 'bookContent');
             title.textContent= library[i].title;
@@ -42,29 +69,53 @@ function createBookCards () {
             pages.classList.add('author', 'bookContent');
             pages.textContent = `${library[i].pages} pages`;
         const readBtn = document.createElement("button");
-            readBtn.textContent = "Read"; 
             readBtn.classList.add('readBtn', 'bookContent');
+            if (library[i].read === true) {
+            readBtn.textContent = "Read"; 
             readBtn.style.backgroundColor = 'green';
+            } else {
+            readBtn.textContent = "Not Read"; 
+            readBtn.style.backgroundColor = 'gray';
+            }
             readBtn.addEventListener ('click', function() {
                 const color = event.target.style.backgroundColor
                 if (color === 'green') {
-                    event.target.style.backgroundColor = 'red';
+                    event.target.style.backgroundColor = 'gray';
                     event.target.textContent = "Not Read";
                 } else {
                     event.target.style.backgroundColor = 'green';
                     event.target.textContent = "Read";
                 }
             })
+        const closeBtn = document.createElement("button");
+            closeBtn.textContent = "Remove";
+            closeBtn.addEventListener('click', function () {
+                let element = document.getElementById(`book-${bookIndex}`);
+                element.remove();
+                library.splice(bookIndex,1);
+                console.log(library);
+            });
         
         container.appendChild(book);
         book.appendChild(title);
         book.appendChild(author);
         book.appendChild(pages);
         book.appendChild(readBtn);
+        book.appendChild(closeBtn);
 
     }
 }
 
+const addBook = (ev) => {
+    ev.preventDefault();
+    const newBook = getBookFromInput();
+    library.push(newBook);
+    resetBookCards();
+    createBookCards();
+    closeForm();
+}
+
+document.getElementById('submit').addEventListener('click',addBook);
 
 
 
